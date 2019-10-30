10124502c1b06cc8e9f12e52752aabe13455bf23
Simon <simonendris@gmail.com>
2019-10-29 21:33:59 -0400

    Displaying JSON data

---
+++ Labs/Displaying JSON/js/app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el:'#app',
+    data() {
+        return {
+            shoes: null
+        }
+    },
+    mounted() {
+        axios
+            .get('json/data.json')
+            .then(response => (this.shoes = response.shoes))
+    }
+})
